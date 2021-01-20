e797492c1256dcfb64efa19a83074395cdfb84e4
Segovia <35817798+SegoCode@users.noreply.github.com>
2021-01-20 13:55:28 +0100

    Add files via upload

---
+++ src/tools/updater.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/tcnksm/go-latest"
+	"gopkg.in/toast.v1"
+)
+
+func main() {
+	githubTag := &latest.GithubTag{
+		Owner:      "SegoCode",
+		Repository: "AutoWall",
+	}
+	res, _ := latest.Check(githubTag, "1.4.0")
+	if res.Outdated {
+		notification := toast.Notification{
+			AppID:               "AutoWall",
+			Title:               "AutoWall",
+			Message:             "A new version is available. Click here to download.",
+			ActivationArguments: "https://github.com/SegoCode/AutoWall/releases",
+		}
+		notification.Push()
+
+	}
+}
