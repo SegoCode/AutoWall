a1b9e4aaab4c520d787f3ef84e391a596f0a0d26
SegoCode <35817798+SegoCode@users.noreply.github.com>
2022-03-03 18:49:14 +0100

    New logic updater in raw golang

--- src/tools/updater.go
+++ src/tools/updater.go
@@ -1,30 +1,59 @@
 package main
 
 import (
-	"github.com/tcnksm/go-latest"
-	"gopkg.in/toast.v1"
+	"encoding/json"
 	"io/ioutil"
-	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"syscall"
+	"unsafe"
 )
 
-func main() {
-	githubTag := &latest.GithubTag{
-		Owner:      "SegoCode",
-		Repository: "AutoWall",
-	}
-	
-	verfile, _ := ioutil.ReadFile("tools\\version.dat")
-	res, _ := latest.Check(githubTag, string(verfile))
+// MessageBox of Win32 API.
+func MessageBox(hwnd uintptr, caption, title string, flags uint) int {
+	ret, _, _ := syscall.NewLazyDLL("user32.dll").NewProc("MessageBoxW").Call(
+		uintptr(hwnd),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(caption))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(title))),
+		uintptr(flags))
 
-	if res.Outdated {
-		log.Print("New version is available. Actual: " + string(verfile) + " Found: " + res.Current)
-		notification := toast.Notification{
-			AppID:               "AutoWall",
-			Title:               "AutoWall",
-			Message:             "A new version is available. Click here to download.",
-			ActivationArguments: "https://github.com/SegoCode/AutoWall/releases",
+	return int(ret)
+}
+
+func main() {
+
+	//Make a get request
+	resp, err := http.Get("https://api.github.com/repos/SegoCode/AutoWall/releases/latest")
+	if err != nil {
+		os.Exit(1)
+	}
+
+	//Read the response body on the line below.
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	//Convert the body to type string
+	JSON := string(body)
+
+	// Unmarshal the JSON string into jsonMap map variable.
+	var jsonMap map[string]interface{}
+	json.Unmarshal([]byte(JSON), &jsonMap)
+
+	//Get actual version
+	verfile, err := ioutil.ReadFile("tools\\version.dat")
+	if err != nil {
+		os.Exit(1)
+	}
+
+	//Check version
+	if string(verfile) != jsonMap["tag_name"] {
+		respD := MessageBox(0, "You are running an old version of AutoWall, the actual version is "+jsonMap["tag_name"].(string)+"\nChanges in the latest version:\n\n"+jsonMap["body"].(string)+"\n\n Do you want download last version?", "AutoWall Updater, new version is available", 4)
+		if respD == 6 {
+			exec.Command("rundll32", "url.dll,FileProtocolHandler", jsonMap["html_url"].(string)).Start()
 		}
-		notification.Push()
 
 	}
 }
